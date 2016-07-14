162677ee5a8dd2cf93ae02377cf6d26fafd2d402
Jo Weiß <joweiss@Jos-MacBook-Pro.local>
2016-07-14 16:46:35 +0200

    Enabled the first version of diyplaying statistics: currently one can see the daily total.

--- lib/database.py
+++ lib/database.py
@@ -4,9 +4,10 @@
 import subprocess
 import re
 import json
+import datetime
 
 
-def get_tasks():
+def get_tasks ():
 	apiData = subprocess.check_output(['osascript', '-e', r'''
 	  set taskList to {"\n"}
 
@@ -40,7 +41,7 @@ def get_tasks():
 	return data
 
 
-def get_active_tasks():
+def get_active_tasks ():
 	api_active_tasks = subprocess.check_output(['osascript', '-e', r'''
 		tell application "Tyme2"
 			set trackedTasks to trackedTaskIDs
@@ -52,3 +53,28 @@ def get_active_tasks():
 		active_tasks.append(taskID)
 
 	return active_tasks
+
+
+def get_daily_total ():
+	daily_total = subprocess.check_output(['osascript', '-e', r'''
+		tell application "Tyme2"
+			set nowInSeconds to the time of the (current date)
+			-- start of the working day is currently at 6 o'clock
+			set secondsSinceWorkingDayStart to (nowInSeconds - 60 * 60 * 6)
+
+			set now to current date
+
+			GetTaskRecordIDs startDate (now - secondsSinceWorkingDayStart) endDate now
+			set todaysRecordIDs to the fetchedTaskRecordIDs
+			set todaysDuration to 0
+			repeat with theRecordID in todaysRecordIDs
+				GetRecordWithID theRecordID
+				set theRecordDuration to the timedDuration of the lastFetchedTaskRecord
+				set todaysDuration to todaysDuration + theRecordDuration
+			end repeat
+
+			return todaysDuration
+		end tell
+		'''])
+
+	return str(datetime.timedelta(seconds=float(daily_total)))
--- tyme.py
+++ tyme.py
@@ -4,7 +4,7 @@
 import sys, subprocess, argparse
 import json
 import re
-from workflow import Workflow, ICON_CLOCK, ICON_SYNC, ICON_NOTE, ICON_ERROR
+from workflow import Workflow, ICON_CLOCK, ICON_SYNC, ICON_NOTE, ICON_ERROR, ICON_INFO
 from lib.database import *
 from lib.utils import *
 from lib.applescript import *
@@ -24,7 +24,8 @@ def main(wf):
 		query = args.query[0]
 
 	# retrieve tasks from cache if available
-	tasks = wf.cached_data('tasks', get_tasks, max_age=(60*60*5))
+	# tasks = wf.cached_data('tasks', get_tasks, max_age=(60*60*5))
+	tasks = wf.cached_data('tasks', get_tasks, max_age=0)
 	# get all active tasks
 	active_tasks = get_active_tasks()
 
@@ -39,16 +40,21 @@ def main(wf):
 	# 							icon=ICON_ERROR)
 	if args.active and not active_tasks:
 			wf.add_item(title="There are no active tasks",
-									valid=False,
-									icon=ICON_NOTE)
+						valid=False,
+						icon=ICON_NOTE)
 	elif args.active:
 		for task in tasks:
 			if task['id'] in active_tasks:
 				wf.add_item(title=task['task'],
-										subtitle=task['project'],
-										arg=task['id'],
-										valid=True,
-										icon=ICON_SYNC)
+							subtitle=task['project'],
+							arg=task['id'],
+							valid=True,
+							icon=ICON_SYNC)
+	elif args.statistics:
+		wf.add_item(title=get_daily_total(),
+					subtitle='Daily Total',
+					valid=False,
+					icon=ICON_INFO)
 	else:
 		for task in tasks:
 			# check if task is active and set corresponding icon		
@@ -60,10 +66,10 @@ def main(wf):
 				ICON = ICON_CLOCK
 
 			wf.add_item(title=task['task'],
-									subtitle=task['project'],
-									arg=task['id'],
-									valid=True,
-									icon=ICON)
+						subtitle=task['project'],
+						arg=task['id'],
+						valid=True,
+						icon=ICON)
 
 	# Send the results to Alfred as XML
 	wf.send_feedback()
@@ -73,6 +79,8 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-a', '--active', action='store_true',
                         help='show active tasks only')
+    parser.add_argument('-stats', '--statistics', action='store_true',
+                        help='show todays statistics')
     parser.add_argument('query', type=unicode, nargs=argparse.REMAINDER, help='query string')
     log.debug(wf.args)
     args = parser.parse_args(wf.args)
